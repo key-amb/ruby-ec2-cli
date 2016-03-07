a269269b2ed0a1de90c4f34f9c20957fd4b04dc9
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-08 08:56:40 +0900

    Add subcommand 'create-ami' to create AMI from an instance

--- lib/ec2-cli.rb
+++ lib/ec2-cli.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk-core'
 require 'thor'
 
-#require 'ec2-cli/image'
+require 'ec2-cli/instance'
 
 class EC2Cli < Thor
   package_name "ec2-cli"
@@ -35,6 +35,32 @@ class EC2Cli < Thor
     cli().stop_instances({
       instance_ids: [options['instance-id']],
       dry_run:      options['dry-run'],
+    })
+  end
+
+  desc 'create-ami', 'Create AMI from an instance'
+  option 'instance-id', :required => true, :aliases => 'i'
+  option 'dry-run', :type => :boolean, :default => false, :aliases => 'n'
+  def create_ami
+    instance = EC2Cli::Instance.fetch(
+      cli: cli(),
+      id:  options['instance-id'],
+    )
+    t = Time.now
+    image_name  = instance.name + t.strftime('.%Y%m%d_%H%M')
+    description = 'Created from %s at %s'%[instance.name, t.to_s]
+    cli().create_image({
+      instance_id: options['instance-id'],
+      name:        image_name,
+      description: description,
+      no_reboot:   true,
+      dry_run:     options['dry-run'],
+      block_device_mappings: [
+        { device_name: '/dev/sdm', virtual_name: 'ephemeral0' },
+        { device_name: '/dev/sdn', virtual_name: 'ephemeral1' },
+        { device_name: '/dev/sdo', virtual_name: 'ephemeral2' },
+        { device_name: '/dev/sdp', virtual_name: 'ephemeral3' },
+      ],
     })
   end
 
