d87ad658abd52071ed9ab1d79d702bdda90791f5
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-07 18:53:48 +0900

    Use thor instead of optparse

--- lib/ec2-cli.rb
+++ lib/ec2-cli.rb
@@ -1,47 +1,46 @@
-require 'optparse'
-require 'ostruct'
+require 'aws-sdk-core'
+require 'thor'
 
-require 'pp'
+#require 'ec2-cli/image'
 
-module EC2Cli
-  class << self
-    def parse_option
-      options = OpenStruct.new
-      OptionParser.new do |opts|
-        opts.on('-i', '--instance-id ID', 'instance-id') do |id|
-          options.instance_id = id
-        end
-        opts.on('-n', '--dry-run', 'Dry-run') do |dr|
-          options.dry_run = true
-        end
-      end.parse!
-      options
+class EC2Cli < Thor
+  package_name "ec2-cli"
+  default_command :list
+
+  desc 'list', 'List instances'
+  def list
+    cli().describe_instances.reservations[0].instances.each do |i|
+      name_tags = i.tags.select { |t| t.key == 'Name' }
+      puts [
+        name_tags[0].value, i.instance_id, i.state.name,
+        i.private_ip_address, i.public_ip_address
+      ].join("\t")
     end
+  end
 
-    def list(cli)
-      cli.describe_instances.reservations[0].instances.each do |i|
-        name_tags = i.tags.select { |t| t.key == 'Name' }
-        puts [
-          name_tags[0].value, i.instance_id, i.state.name,
-          i.private_ip_address, i.public_ip_address
-        ].join("\t")
-      end
-    end
+  desc 'start', 'Start an instance'
+  option 'instance-id', :required => true, :aliases => 'i'
+  option 'dry-run', :type => :boolean, :default => false, :aliases => 'n'
+  def start
+    cli().start_instances({
+      instance_ids: [options['instance-id']],
+      dry_run:      options['dry-run'],
+    })
+  end
 
-    def start(cli)
-      options = parse_option
-      cli.start_instances({
-        instance_ids: [options.instance_id],
-        dry_run:      options.dry_run,
-      })
-    end
+  desc 'stop', 'Stop an instance'
+  option 'instance-id', :required => true, :aliases => 'i'
+  option 'dry-run', :type => :boolean, :default => false, :aliases => 'n'
+  def stop
+    cli().stop_instances({
+      instance_ids: [options['instance-id']],
+      dry_run:      options['dry-run'],
+    })
+  end
 
-    def stop(cli)
-      options = parse_option
-      cli.stop_instances({
-        instance_ids: [options.instance_id],
-        dry_run:      options.dry_run,
-      })
-    end
+  private
+
+  def cli
+    @cli ||= Aws::EC2::Client.new
   end
 end
