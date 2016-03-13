471005d31185a0e005176ed86cdb94438836923c
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-13 11:08:22 +0900

    Add tests for EC2It::Config

--- lib/ec2it/config.rb
+++ lib/ec2it/config.rb
@@ -1,4 +1,5 @@
 require 'singleton'
+require 'thor'
 
 class EC2It < Thor
   class Config
---
+++ spec/config.rb
@@ -0,0 +1,100 @@
+require 'ec2it/config'
+
+require 'tempfile'
+require 'toml'
+
+toml_content = <<"EOS"
+account_id = "123456789012"
+
+[instance]
+default_instance_type  = "t2.micro"
+default_security_group = "sg-xxxxxxxx"
+
+[vpc]
+default_availability_zone = "ap-northeast-1c"
+
+[tags]
+roles  = [ "Feature", "Role"  ]
+groups = [ "Service", "Group" ]
+EOS
+
+config_stash = TOML.parse(toml_content)
+
+tmp = Tempfile.open('tmp') do |fp|
+  fp.puts toml_content
+  fp
+end
+
+class EC2It::Config
+  def me
+    @me
+  end
+end
+
+describe 'EC2It::Config' do
+  ENV['EC2IT_CONFIG_PATH'] = tmp.path
+  config = EC2It::Config.instance
+
+  describe 'Config initialization' do
+    it 'Match with TOML' do
+      expect(config.me).to match config_stash
+    end
+
+    config_stash.each_pair do |key, value|
+      it "Top level key '#{key}' is readable accessor" do
+        expect( config.send(key) ).to match value
+      end
+    end
+  end
+
+  require 'ostruct'
+  describe 'tags2params' do
+    tags_hashes = [
+      [ { key: 'Feature', value: 'web' } ],
+      [
+        { key: 'Feature', value: 'db' },
+        { key: 'Role',    value: 'master' },
+        { key: 'Service', value: 'myapp' },
+      ],
+    ]
+    tags_list = []
+    tags_hashes.each do |ths|
+      tags = []
+      ths.each do |i|
+        tags.push(OpenStruct.new i)
+      end
+      tags_list.push(tags)
+    end
+
+    params = [
+      { role: 'web',       group: '' },
+      { role: 'db:master', group: 'myapp' },
+    ]
+    (0..1).each do |i|
+      it params[i].to_s do
+        expect(config.tags2params(tags_list[i])).to match(params[i])
+      end
+    end
+  end
+
+  describe 'params2tagfilters' do
+    args_list = [
+      { role: 'web',       group: '' },
+      { role: 'db:master', group: 'myapp' },
+    ]
+    filters_list = [
+      [ { name: 'tag:Feature', values: ['web'] } ],
+      [
+        { name: 'tag:Feature', values: ['db'] },
+        { name: 'tag:Role',    values: ['master'] },
+        { name: 'tag:Service', values: ['myapp'] },
+      ],
+    ]
+
+    (0..1).each do |i|
+      it args_list[i].to_s do
+        expect(config.params2tagfilters(args_list[i])).to match(filters_list[i])
+      end
+    end
+  end
+end
