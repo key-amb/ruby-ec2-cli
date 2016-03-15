0882741e2411fd59167d5cd96e144986da32491c
key-amb <yasutake.kiyoshi@gmail.com>
2016-03-15 14:32:01 +0900

    Features 'set-role', 'set-group' and refactor and improvements
    - 'set-role' to add or update role of instance or AMI
    - 'set-group' to add or update group of instance or AMI
    - Change Config from Singleton to normal class

--- lib/ec2it/ami.rb
+++ lib/ec2it/ami.rb
@@ -1,71 +1,71 @@
-class EC2It < Thor
-  class AMI
-    accessors = [
-      :image_id, :image_name, :name, :role, :group, :status, :base_name,
-      :disp_name, :disp_role, :disp_group, :disp_info, :described
-    ]
-    accessors.each do |acsr|
-      attr acsr
+require 'ec2it'
+
+class EC2It::AMI
+  accessors = [
+    :image_id, :image_name, :name, :role, :group, :status, :base_name,
+    :disp_name, :disp_role, :disp_group, :disp_info, :described
+  ]
+  accessors.each do |acsr|
+    attr acsr
+  end
+
+  def initialize(params)
+    params.each do |key, val|
+      instance_variable_set("@#{key}", val)
+    end
+    @disp_name  = @name || @image_name
+    @disp_role  = @role.length > 0 ? @role : 'nil'
+    @disp_group = @group.length > 0 ? @group : 'nil'
+    @disp_info  = '%s:%s(%s){%s}'%[@disp_name, @status, @disp_role, @disp_group]
+  end
+
+  def self.fetch_by_id(id, cli: nil)
+    raise 'No id specified!' unless id
+    resp = cli.describe_images({
+      image_ids: [id]
+    })
+    i = resp.images[0]
+    new( Util.prepare_image_params(i) )
+  end
+
+  def self.fetch(cli: nil, role: nil, group: nil)
+    config = EC2It::Config.get_or_new
+
+    args = {
+      owners: [ config.account_id ],
+    }
+    config.params2tagfilters(role: role, group: group).tap do |f|
+      args['filters'] = f if f.length > 0
     end
 
-    def initialize(params)
-      params.each do |key, val|
-        instance_variable_set("@#{key}", val)
-      end
-      @disp_name  = @name || @image_name
-      @disp_role  = @role.length > 0 ? @role : 'nil'
-      @disp_group = @group.length > 0 ? @group : 'nil'
-      @disp_info  = '%s:%s(%s){%s}'%[@disp_name, @status, @disp_role, @disp_group]
+    results = []
+    cli.describe_images(args).images.each do |i|
+      results.push(
+        new( Util.prepare_image_params(i, config) )
+      )
     end
 
-    def self.fetch_by_id(id: nil, cli: nil)
-      raise 'No id specified!' unless id
-      resp = cli.describe_images({
-        image_ids: [id]
+    results
+  end
+
+  def snapshot_id
+    return @snapshot_id if @snapshot_id
+    self.described.block_device_mappings.each do |bdm|
+      next unless bdm.ebs
+      return @snapshot_id = bdm.ebs.snapshot_id
+    end
+  end
+
+  module Util
+    module_function
+    def prepare_image_params(image, config=nil)
+      config ||= EC2It::Config.get_or_new
+      params = config.tags2params(image.tags).merge({
+        image_id:   image.image_id,
+        image_name: image.name,
+        status:     image.state,
+        described:  image,
       })
-      i = resp.images[0]
-      new( Util.prepare_image_params(i) )
-    end
-
-    def self.fetch(cli: nil, role: nil, group: nil)
-      config = EC2It::Config.instance
-
-      args = {
-        owners: [ config.account_id ],
-      }
-      config.params2tagfilters(role: role, group: group).tap do |f|
-        args['filters'] = f if f.length > 0
-      end
-
-      results = []
-      cli.describe_images(args).images.each do |i|
-        results.push(
-          new( Util.prepare_image_params(i, config) )
-        )
-      end
-
-      results
-    end
-
-    def snapshot_id
-      return @snapshot_id if @snapshot_id
-      self.described.block_device_mappings.each do |bdm|
-        next unless bdm.ebs
-        return @snapshot_id = bdm.ebs.snapshot_id
-      end
-    end
-
-    module Util
-      module_function
-      def prepare_image_params(image, config=nil)
-        config ||= EC2It::Config.instance
-        params = config.tags2params(image.tags).merge({
-          image_id:   image.image_id,
-          image_name: image.name,
-          status:     image.state,
-          described:  image,
-        })
-      end
     end
   end
 end
--- lib/ec2it/instance.rb
+++ lib/ec2it/instance.rb
@@ -1,89 +1,89 @@
-class EC2It < Thor
-  class Instance
-    accessors = [
-      :instance_id, :name, :role, :group, :status, :ipaddress, :public_ipaddress,
-      :instance_type, :disp_name, :disp_role, :disp_group, :disp_info, :described,
-    ]
-    accessors.each do |acsr|
-      attr acsr
-    end
+require 'ec2it'
 
-    def initialize(params)
-      params.each do |key, val|
-        instance_variable_set("@#{key}", val)
+class EC2It::Instance
+  accessors = [
+    :instance_id, :name, :role, :group, :status, :ipaddress, :public_ipaddress,
+    :instance_type, :disp_name, :disp_role, :disp_group, :disp_info, :described,
+  ]
+  accessors.each do |acsr|
+    attr acsr
+  end
+
+  def initialize(params)
+    params.each do |key, val|
+      instance_variable_set("@#{key}", val)
+    end
+    @disp_name  = @name.length > 0 ? @name : 'nil'
+    @disp_role  = @role.length > 0 ? @role : 'nil'
+    @disp_group = @group.length > 0 ? @group : 'nil'
+    @disp_info  = '%s:%s(%s){%s}'%[@disp_name, @status, @disp_role, @disp_group]
+  end
+
+  def self.fetch_one(cli: nil, id: nil, name: nil)
+    if id
+      fetch_by_id(id, cli: cli)
+    elsif name
+      fetch_by_name(name, cli: cli)
+    else
+      raise 'No id nor name specified! Please specify one.'
+    end
+  end
+
+  def self.fetch_by_id(id, cli: nil)
+    raise 'No id specified!' unless id
+    resp = cli.describe_instances({
+      instance_ids: [id]
+    })
+    i = resp.reservations[0].instances[0]
+    new( Util.prepare_instance_params(i) )
+  end
+
+  def self.fetch_by_name(name, cli: nil)
+    raise 'No name specified!' unless name
+    resp = cli.describe_instances({
+      filters: [{ name: 'tag:Name', values: [name] }],
+    })
+    i = resp.reservations[0].instances[0]
+    new( Util.prepare_instance_params(i) )
+  end
+
+  def self.fetch(cli: nil, role: nil, group: nil, status: nil)
+    config = EC2It::Config.get_or_new
+
+    args = {}
+    filters = []
+    config.params2tagfilters(role: role, group: group).tap do |f|
+      filters.concat(f)
+    end
+    if status
+      filters.push({ name: 'instance-state-name', values: [status] })
+    end
+    args['filters'] = filters if filters.length > 0
+
+    results = []
+    cli.describe_instances(args).reservations.each do |r|
+      r.instances.each do |i|
+        results.push(
+          new( Util.prepare_instance_params(i, config) )
+        )
       end
-      @disp_name  = @name.length > 0 ? @name : 'nil'
-      @disp_role  = @role.length > 0 ? @role : 'nil'
-      @disp_group = @group.length > 0 ? @group : 'nil'
-      @disp_info  = '%s:%s(%s){%s}'%[@disp_name, @status, @disp_role, @disp_group]
     end
 
-    def self.fetch_one(cli: nil, id: nil, name: nil)
-      if id
-        return fetch_by_id(cli: cli, id: id)
-      elsif name
-        return fetch_by_name(cli: cli, name: name)
-      else
-        raise 'No id nor name specified! Please specify one.'
-      end
-    end
+    results
+  end
 
-    def self.fetch_by_id(id: nil, cli: nil)
-      raise 'No id specified!' unless id
-      resp = cli.describe_instances({
-        instance_ids: [id]
+  module Util
+    module_function
+    def prepare_instance_params(instance, config=nil)
+      config ||= EC2It::Config.get_or_new
+      params = config.tags2params(instance.tags).merge({
+        instance_id:      instance.instance_id,
+        status:           instance.state.name,
+        ipaddress:        instance.private_ip_address,
+        public_ipaddress: instance.public_ip_address,
+        instance_type:    instance.instance_type,
+        described:        instance,
       })
-      i = resp.reservations[0].instances[0]
-      new( Util.prepare_instance_params(i) )
-    end
-
-    def self.fetch_by_name(name: nil, cli: nil)
-      raise 'No name specified!' unless name
-      resp = cli.describe_instances({
-        filters: [{ name: 'tag:Name', values: [name] }],
-      })
-      i = resp.reservations[0].instances[0]
-      new( Util.prepare_instance_params(i) )
-    end
-
-    def self.fetch(cli: nil, role: nil, group: nil, status: nil)
-      config = EC2It::Config.instance
-
-      args = {}
-      filters = []
-      config.params2tagfilters(role: role, group: group).tap do |f|
-        filters.concat(f)
-      end
-      if status
-        filters.push({ name: 'instance-state-name', values: [status] })
-      end
-      args['filters'] = filters if filters.length > 0
-
-      results = []
-      cli.describe_instances(args).reservations.each do |r|
-        r.instances.each do |i|
-          results.push(
-            new( Util.prepare_instance_params(i, config) )
-          )
-        end
-      end
-
-      results
-    end
-
-    module Util
-      module_function
-      def prepare_instance_params(instance, config=nil)
-        config ||= EC2It::Config.instance
-        params = config.tags2params(instance.tags).merge({
-          instance_id:      instance.instance_id,
-          status:           instance.state.name,
-          ipaddress:        instance.private_ip_address,
-          public_ipaddress: instance.public_ip_address,
-          instance_type:    instance.instance_type,
-          described:        instance,
-        })
-      end
     end
   end
 end
